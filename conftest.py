9070bae3d218490a1749ee92dcafd54652f3eebf
Marc Mauri <marcmaurialloza@gmail.com>
2021-11-29 23:58:50 +0100

    Fix conftest to be pep8 compliant

--- conftest.py
+++ conftest.py
@@ -2,5 +2,6 @@
 import platform
 
 collect_ignore = ["setup.py"]
-if int(platform.python_version_tuple()[0]) >= 3 and int(platform.python_version_tuple()[1]) >= 8:
+if (int(platform.python_version_tuple()[0]) >= 3 and
+        int(platform.python_version_tuple()[1]) >= 8):
     collect_ignore_glob = ["*_py3.8.py"]
