451090bc1551579fdb479897befab1a89fd2271f
opengeekv2 <Fr33s1ng1ng33k>
2021-11-29 16:09:33 +0100

    Remove tests that depend on python 3.8

--- conftest.py
+++ conftest.py
@@ -2,5 +2,5 @@
 import platform
 
 collect_ignore = ["setup.py"]
-if float('.'.join(platform.python_version_tuple()[0:2])) >= 2.8:
-    collect_ignore_glob = ["*_py2.8.py"]
+if float('.'.join(platform.python_version_tuple()[0:2])) < 3.8:
+    collect_ignore_glob = ["*_py3.8.py"]
--- tests/bite_di/test_bite_inject_to_functions.py
+++ tests/bite_di/test_bite_inject_to_functions.py
@@ -1,5 +1,4 @@
 import pytest
-from platform import python_version_tuple
 from bite_di import container, inject
 
 
@@ -249,70 +248,3 @@ def test_replaces_args_and_kwargs() -> None:
 
     assert (HOLA + ' ' + NAME + ' ' + HELLO + ' ' + OTHER_NAME
             == greet(HOLA, NAME, other_greeting=HELLO, other_name=OTHER_NAME))
-
-
-if float('.'.join(python_version_tuple()[0:2])) >= 3.8:
-    def test_all_types_of_params():
-
-        contents = {
-            'posonly': 1,
-            'pos': 2,
-            'fulltuple': (1, 2),
-            'kwonly': 'hola',
-            'fulldict': {
-                'greeting': 'hola',
-            }
-        }
-        container(contents)
-
-        @inject
-        def difficult(posonly, /, pos, *fulltuple, kwonly=None, **fulldict):
-            return {
-                'posonly': posonly,
-                'pos': pos,
-                'fulltuple': fulltuple,
-                'kwonly': kwonly,
-                'fulldict': fulldict
-            }
-        result = difficult()
-
-        assert contents == result
-
-    def test_all_types_of_params_with_extras():
-
-        contents = {
-            'posonly': 1,
-            'pos': 2,
-            'fulltuple': (1, 2),
-            'kwonly': 'hola',
-            'fulldict': {
-                'greeting': 'hola',
-            },
-            'name': 'Michael Palin'
-        }
-        container(contents)
-
-        @inject
-        def difficult(posonly, /, pos, *fulltuple, kwonly=None, **fulldict):
-            return {
-                'posonly': posonly,
-                'pos': pos,
-                'fulltuple': fulltuple,
-                'kwonly': kwonly,
-                'fulldict': fulldict,
-                'name': fulldict['name']
-            }
-        result = difficult(3, 4, 5, 6, kwonly='adeu', name=None)
-        expected = {
-            'posonly': 3,
-            'pos': 4,
-            'fulltuple': (5, 6, 1, 2),
-            'kwonly': 'adeu',
-            'fulldict': {
-                'greeting': 'hola',
-                'name': 'Michael Palin'
-            },
-            'name': 'Michael Palin'
-        }
-
-        assert expected == result
