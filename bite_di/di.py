23fb5fd89ff87ddbb6f999ce6e25f8fa2e7609b3
opengeekv2 <Fr33s1ng1ng33k>
2021-11-29 16:37:44 +0100

    Fix unsupported type hint in python < 3.9

--- bite_di/di.py
+++ bite_di/di.py
@@ -1,8 +1,9 @@
 from inspect import getfullargspec, signature, Parameter, Signature
 from functools import wraps
-from typing import Callable
+from typing import Callable, Dict
 
-def _replace_args_by_string(args: tuple, kwargs: dict, argspec: list[str], contents: dict) -> tuple:
+
+def _replace_args_by_string(args: tuple, kwargs: Dict, argspec: list[str], contents: Dict) -> tuple:
     arglist = list(args)
     for i, arg in enumerate(argspec):
         if arg not in kwargs.keys():
@@ -11,10 +12,12 @@ def _replace_args_by_string(args: tuple, kwargs: dict, argspec: list[str], conte
                 arglist.append(parameter_to_inject)
     return tuple(arglist)
 
-def _merge_varargs(args: tuple, varargs: str, contents: dict) -> tuple:
+
+def _merge_varargs(args: tuple, varargs: str, contents: Dict) -> tuple:
     return args + contents.get(varargs, ())
 
-def _replace_kwonlyargs(kwargs: dict, kwonlyargs: list[str], contents: dict) -> dict:
+
+def _replace_kwonlyargs(kwargs: Dict, kwonlyargs: list[str], contents: Dict) -> Dict:
     for kwonlyarg in kwonlyargs:
         if kwonlyarg not in kwargs.keys():
             parameter_to_inject = contents.get(kwonlyarg, None)
@@ -22,37 +25,45 @@ def _replace_kwonlyargs(kwargs: dict, kwonlyargs: list[str], contents: dict) -> 
                 kwargs[kwonlyarg] = parameter_to_inject
     return kwargs
 
-def _merge_named_kwargs(kwargs: dict, varkw: str, contents: dict) -> dict:
+
+def _merge_named_kwargs(kwargs: Dict, varkw: str, contents: Dict) -> Dict:
     kwargs.update(contents.get(varkw, {}).copy())
     return kwargs
 
-def _replace_kwargs(kwargs: dict, kwonlyargs: list[str], contents: dict) -> dict:
+
+def _replace_kwargs(kwargs: Dict, kwonlyargs: list[str], contents: Dict) -> Dict:
     for k, v in kwargs.items():
         if v is None and k not in kwonlyargs and contents.get(k, None) is not None:
             kwargs[k] = contents[k]
     return kwargs
 
+
 def create_container():
-    def container(new: dict={}, contents: dict={}, decorated: list = []):
+    def container(new: Dict = {}, contents: Dict = {}, decorated: list = []):
         if new:
             contents.update(new)
 
         def dump():
             print(contents)
-        
+
         def inject(func):
             decorated.append(func)
+
             @wraps(func)
             def wrapper(*args, **kwargs):
                 fullargspec = getfullargspec(func)
-                args = _replace_args_by_string(args, kwargs, fullargspec.args, contents)
+                args = _replace_args_by_string(
+                    args, kwargs, fullargspec.args, contents)
                 args = _merge_varargs(args, fullargspec.varargs, contents)
-                kwargs = _merge_named_kwargs(kwargs, fullargspec.varkw, contents)
-                kwargs = _replace_kwonlyargs(kwargs, fullargspec.kwonlyargs, contents)
-                kwargs = _replace_kwargs(kwargs, fullargspec.kwonlyargs, contents)
+                kwargs = _merge_named_kwargs(
+                    kwargs, fullargspec.varkw, contents)
+                kwargs = _replace_kwonlyargs(
+                    kwargs, fullargspec.kwonlyargs, contents)
+                kwargs = _replace_kwargs(
+                    kwargs, fullargspec.kwonlyargs, contents)
 
                 return func(*args, **kwargs)
-            
+
             return wrapper
 
         class Container:
@@ -73,13 +84,12 @@ def create_container():
                 s = signature(f)
                 call_signature = s.replace()
                 for p in s.parameters.values():
-                    call_signature = s.replace(parameters=[p.replace(annotation=Parameter.empty)], return_annotation=Signature.empty)
+                    call_signature = s.replace(parameters=[p.replace(
+                        annotation=Parameter.empty)], return_annotation=Signature.empty)
                 call = str(call_signature)
                 for key in s.parameters.keys():
                     call.replace(key, str(contents.get(key)))
                 return f.__name__ + call
-                
 
         return Container(inject, dump, decorated)
     return container
-
