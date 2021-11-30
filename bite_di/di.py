61e9e0d392fff2c58b0449888024c3aa6facabb3
opengeekv2 <Fr33s1ng1ng33k>
2021-11-30 13:52:53 +0100

    Improve types coverage

--- bite_di/di.py
+++ bite_di/di.py
@@ -1,11 +1,11 @@
 from inspect import getfullargspec, signature, Parameter, Signature
 from functools import wraps
-from typing import Callable, List
+from typing import Callable, List, Dict, Tuple, Any
 
 
 def _replace_args_by_string(
-        args: tuple, kwargs: dict, argspec: List[str],
-        contents: dict) -> tuple:
+        args: Tuple[Any, ...], kwargs: Dict[str, Any], argspec: List[str],
+        contents: Dict[str, Any]) -> tuple:
     arglist = list(args)
     for i, arg in enumerate(argspec):
         if arg not in kwargs.keys():
@@ -15,13 +15,15 @@ def _replace_args_by_string(
     return tuple(arglist)
 
 
-def _merge_varargs(args: tuple, varargs: str, contents: dict) -> tuple:
+def _merge_varargs(
+        args: Tuple[Any, ...], varargs: str,
+        contents: Dict[str, Any]) -> tuple:
     return args + contents.get(varargs, ())
 
 
 def _replace_kwonlyargs(
-        kwargs: dict, kwonlyargs: List[str],
-        contents: dict) -> dict:
+        kwargs: Dict[str, Any], kwonlyargs: List[str],
+        contents: Dict[str, Any]) -> Dict[str, Any]:
     for kwonlyarg in kwonlyargs:
         if kwonlyarg not in kwargs.keys():
             parameter_to_inject = contents.get(kwonlyarg, None)
@@ -30,14 +32,16 @@ def _replace_kwonlyargs(
     return kwargs
 
 
-def _merge_named_kwargs(kwargs: dict, varkw: str, contents: dict) -> dict:
+def _merge_named_kwargs(
+        kwargs: Dict[str, Any], varkw: str,
+        contents: Dict[str, Any]) -> Dict[str, Any]:
     kwargs.update(contents.get(varkw, {}).copy())
     return kwargs
 
 
 def _replace_kwargs(
-        kwargs: dict, kwonlyargs: List[str],
-        contents: dict) -> dict:
+        kwargs: Dict[str, Any], kwonlyargs: List[str],
+        contents: Dict[str, Any]) -> Dict[str, Any]:
     for k, v in kwargs.items():
         if v is None and k not in kwonlyargs and contents.get(
                 k, None) is not None:
@@ -47,19 +51,19 @@ def _replace_kwargs(
 
 def create_container() -> Callable:
     def container(
-            new: dict = {}, contents: dict = {},
-            decorated: list = []) -> Callable:
+            new: Dict[str, Any] = {}, contents: Dict[str, Any] = {},
+            decorated: List = [Callable]) -> Callable:
         if new:
             contents.update(new)
 
         def dump() -> None:
             print(contents)
 
-        def inject(func) -> Callable:
+        def inject(func: Callable) -> Callable:
             decorated.append(func)
 
             @wraps(func)
-            def wrapper(*args: list, **kwargs: dict) -> Callable:
+            def wrapper(*args: Any, **kwargs: Dict[str, Any]) -> Any:
                 fullargspec = getfullargspec(func)
                 args = _replace_args_by_string(
                     args, kwargs, fullargspec.args, contents)
@@ -88,10 +92,10 @@ def create_container() -> Callable:
                 self.dump = dump
                 self.decorated = decorated
 
-            def __call__(self) -> tuple:
+            def __call__(self) -> Tuple[Callable, Callable]:
                 return (self.inject, self.dump)
 
-            def generate_test_call(self, f):
+            def generate_test_call(self, f: Callable) -> str:
                 s = signature(f)
                 call_signature = s.replace()
                 for p in s.parameters.values():
