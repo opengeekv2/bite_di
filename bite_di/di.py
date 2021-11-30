1a2173de68a56d92da5c396373096f4ec815df18
Marc Mauri Alloza <marcmaurialloza@gmail.com>
2021-11-30 14:23:46 +0100

    Improve type coverage (#6)
    * Improve type coverage
    
    * Fix linting issues
    
    * Try to fix lintly issues
    
    * Improve types coverage
    
    * Delete unused code
    
    * Remove unsed imports

--- bite_di/di.py
+++ bite_di/di.py
@@ -1,11 +1,11 @@
-from inspect import getfullargspec, signature, Parameter, Signature
+from inspect import getfullargspec
 from functools import wraps
-from typing import Callable, List
+from typing import Callable, List, Dict, Tuple, Any
 
 
 def _replace_args_by_string(
-        args: tuple, kwargs: dict, argspec: List[str],
-        contents: dict) -> tuple:
+        args: Tuple[Any, ...], kwargs: Dict[str, Any], argspec: List[str],
+        contents: Dict[str, Any]) -> tuple:
     arglist = list(args)
     for i, arg in enumerate(argspec):
         if arg not in kwargs.keys():
@@ -15,13 +15,15 @@ def _replace_args_by_string(
     return tuple(arglist)
 
 
-def _merge_varargs(args: tuple, varargs: str, contents: dict) -> tuple:
+def _merge_varargs(
+        args: Tuple[Any, ...], varargs: str,
+        contents: Dict[str, Any]) -> tuple:
     return args + contents.get(varargs, ())
 
 
 def _replace_kwonlyargs(
-        kwargs: dict, kwonlyargs: List[str],
-        contents: dict) -> dict:
+        kwargs: Dict[str, Any], kwonlyargs: List[str],
+        contents: Dict[str, Any]) -> Dict[str, Any]:
     for kwonlyarg in kwonlyargs:
         if kwonlyarg not in kwargs.keys():
             parameter_to_inject = contents.get(kwonlyarg, None)
@@ -30,14 +32,16 @@ def _replace_kwonlyargs(
     return kwargs
 
 
-def _merge_named_kwargs(kwargs: dict, varkw: str, contents: dict) -> dict:
+def _merge_named_kwargs(
+        kwargs: Dict[str, Any], varkw: str,
+        contents: Dict[str, Any]) -> Dict[str, Any]:
     kwargs.update(contents.get(varkw, {}).copy())
     return kwargs
 
 
 def _replace_kwargs(
-        kwargs: dict, kwonlyargs: List[str],
-        contents: dict) -> dict:
+        kwargs: Dict[str, Any], kwonlyargs: List[str],
+        contents: Dict[str, Any]) -> Dict[str, Any]:
     for k, v in kwargs.items():
         if v is None and k not in kwonlyargs and contents.get(
                 k, None) is not None:
@@ -45,25 +49,29 @@ def _replace_kwargs(
     return kwargs
 
 
-def create_container():
-    def container(new: dict = {}, contents: dict = {}, decorated: list = []):
+def create_container() -> Callable:
+    def container(
+            new: Dict[str, Any] = {}, contents: Dict[str, Any] = {},
+            decorated: List = [Callable]) -> Callable:
         if new:
             contents.update(new)
 
-        def dump():
+        def dump() -> None:
             print(contents)
 
-        def inject(func):
+        def inject(func: Callable) -> Callable:
             decorated.append(func)
 
             @wraps(func)
-            def wrapper(*args, **kwargs):
+            def wrapper(*args: Any, **kwargs: Dict[str, Any]) -> Any:
                 fullargspec = getfullargspec(func)
                 args = _replace_args_by_string(
                     args, kwargs, fullargspec.args, contents)
-                args = _merge_varargs(args, fullargspec.varargs, contents)
-                kwargs = _merge_named_kwargs(
-                    kwargs, fullargspec.varkw, contents)
+                if fullargspec.varargs is not None:
+                    args = _merge_varargs(args, fullargspec.varargs, contents)
+                if fullargspec.varkw is not None:
+                    kwargs = _merge_named_kwargs(
+                        kwargs, fullargspec.varkw, contents)
                 kwargs = _replace_kwonlyargs(
                     kwargs, fullargspec.kwonlyargs, contents)
                 kwargs = _replace_kwargs(
@@ -84,20 +92,8 @@ def create_container():
                 self.dump = dump
                 self.decorated = decorated
 
-            def __call__(self) -> tuple:
+            def __call__(self) -> Tuple[Callable, Callable]:
                 return (self.inject, self.dump)
-
-            def generate_test_call(self, f):
-                s = signature(f)
-                call_signature = s.replace()
-                for p in s.parameters.values():
-                    call_signature = s.replace(
-                        parameters=[p.replace(annotation=Parameter.empty)],
-                        return_annotation=Signature.empty)
-                call = str(call_signature)
-                for key in s.parameters.keys():
-                    call.replace(key, str(contents.get(key)))
-                return f.__name__ + call
 
         return Container(inject, dump, decorated)
     return container
