d33c80db00d02921e7d66e96edde404c5258480c
opengeekv2 <marcmaurialloza@gmail.com>
2021-12-03 13:05:41 +0100

    Fix conditional testing execution

--- conftest.py
+++ conftest.py
@@ -2,6 +2,6 @@
 import platform
 
 collect_ignore = ["setup.py"]
-if (int(platform.python_version_tuple()[0]) < 3 and
+if (int(platform.python_version_tuple()[0]) < 3 or
         int(platform.python_version_tuple()[1]) < 8):
     collect_ignore_glob = ["*_py38.py"]
